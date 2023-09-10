9e97988449847699dae8cae34174e76fd590ae8f
malekinho8 <shmeek8@gmail.com>
2023-09-10 12:50:37 -0400

    Added functionality for using ChatGPT to summarize pdf text information into a digestible easy to listen form before converting to speech

--- pdf2Speech/main.py
+++ pdf2Speech/main.py
@@ -1,15 +1,22 @@
 import sys; sys.path.append('..')
 import click
 import os
-from utils import (pdf2Image, convertImagesToText, getSentencesFromTextFile,
-                   convertTextToSpeech, getFilenameWithoutExtension, genderStringToGCloudGenderFormat,
-                   AUDIO_DIR,TXT_DIR,IMAGES_DIR)
+import fitz
+from tqdm import tqdm
+from threading import Thread
+from utils import (getSentencesAndConvertToSpeech, process_page,
+                   getFilenameWithoutExtension, genderStringToGCloudGenderFormat,
+                   calculateMoneySpent, AUDIO_DIR, TXT_DIR)
+
+# GPT Model Type
+GPT_MODEL_TYPE = "gpt-3.5-turbo"
 
 @click.command()
 @click.option('--pdf-path', type=click.Path(exists=True), required=True, help='Path to the input PDF file.')
 @click.option('--voice-type', default="en-US-Studio-O", help='Type of voice for the speech.')
 @click.option('--language', default="en-US", help='Language of the speech.')
 @click.option('--gender', default='Female', help='Gender of the voice (Male or Female).')
+
 def pdf2Speech(pdf_path, voice_type, language, gender):
     """Convert a PDF to Speech."""
     # convert gender to proper format
@@ -18,19 +25,50 @@ def pdf2Speech(pdf_path, voice_type, language, gender):
     # get base filename
     base_filename = getFilenameWithoutExtension(pdf_path)
     
-    # PDF to Images
-    image_files = pdf2Image(pdf_path)
-    
-    # Images to Text
-    convertImagesToText(image_files, base_filename)
-    
-    # Get Sentences
-    sentences = getSentencesFromTextFile(os.path.join(TXT_DIR, "out_text_"+base_filename+".txt"))
-    
-    # Text to Speech
-    convertTextToSpeech(sentences, base_filename, voice_type, gender, language)
+    # load the PDF
+    doc = fitz.open(pdf_path)
 
-    click.echo(f"Converted {pdf_path} to audio and saved as {AUDIO_DIR}{base_filename}.mp3.")
+    # loop initialization
+    speech_thread = None
+    section_title = "Preface"
+    input_token_count = 0
+    output_token_count = 0
+    text = ""
+
+    # Use tqdm to create a progress bar
+    for page_index in tqdm(range(len(doc)), desc="Processing pages"):
+        # Process each page of the PDF
+        updated_text, section_title, num_input_tokens, num_output_tokens = process_page(page_index, doc, section_title, GPT_MODEL_TYPE)
+
+        # If there's a previous speech thread, wait for it to finish
+        if speech_thread and speech_thread.is_alive():
+            speech_thread.join()
+        
+        # On another thread, convert the processed text to speech
+        speech_thread = Thread(target=getSentencesAndConvertToSpeech, args=(updated_text, base_filename, voice_type, gender, language))
+        speech_thread.start()
+
+        # Add to the total number of input and output tokens
+        input_token_count += num_input_tokens
+        output_token_count += num_output_tokens
+
+        # Add the updated text to the text variable
+        text += updated_text
+
+    # Ensure the last speech thread finishes before exiting the function
+    if speech_thread:
+        speech_thread.join()
+
+    # Save the text to a file
+    with open(f"{TXT_DIR}out_text_{base_filename}.txt", "w") as f:
+        f.write(text)
+    
+    # Print the number of input and output tokens
+    print(f"Number of input tokens: {input_token_count}")
+    print(f"Number of output tokens: {output_token_count}")
+    print(f"Money spent: ${calculateMoneySpent(input_token_count, output_token_count,GPT_MODEL_TYPE):.2f}")
+
+    click.echo(f"Converted {pdf_path} to audio and saved as {AUDIO_DIR}{base_filename}.mp3!")
 
 if __name__ == "__main__":
     pdf2Speech()
--- pdf2Speech/utils.py
+++ pdf2Speech/utils.py
@@ -2,6 +2,9 @@ import pytesseract
 import os
 import nltk
 import re
+import openai
+import fitz
+import tiktoken
 from nltk.tokenize import sent_tokenize, word_tokenize
 from PIL import Image 
 from pdf2image import convert_from_path 
@@ -14,6 +17,7 @@ nltk.download('punkt')
 
 # utility to create dir
 def createDir(dirName):
+  """Create a directory if it does not exist"""
   if not os.path.exists(dirName):
     os.makedirs(dirName)
     print("Directory " , dirName ,  " Created ")
@@ -22,13 +26,15 @@ def createDir(dirName):
 
 # utility to extract file name
 def getFilenameWithoutExtension(file_path):
-    file_basename = os.path.basename(file_path)
-    filename_without_extension = file_basename.split('.')[0]
-    return filename_without_extension
+  """Extract filename without extension from a file path"""
+  file_basename = os.path.basename(file_path)
+  filename_without_extension = file_basename.split('.')[0]
+  return filename_without_extension
 
 # utility to get percentage of progress
 def getPercentage(current, total):
-    return (current / total) * 100
+  """Get percentage of progress"""
+  return (current / total) * 100
 
 # constants
 IMAGES_DIR = "./content/Images/"
@@ -73,6 +79,7 @@ SECTION_TITLES = [
 
 # Convert a PDF to image snapshots
 def pdf2Image(fn):
+  """Convert a PDF to image snapshots of each page and return the filenames of the images"""
   file = getFilenameWithoutExtension(fn)
   image_fn_dir= IMAGES_DIR + file
   createDir(image_fn_dir)
@@ -113,6 +120,7 @@ def pdf2Image(fn):
 
 # Convert images to text using OCR
 def convertImagesToText(filenames, ofilename):
+  """Convert images to text using OCR and return the filename of the text file"""
   # Creating a text file to write the output 
   outfile = os.path.join(TXT_DIR, "out_text_"+ofilename+".txt")
 
@@ -132,14 +140,20 @@ def convertImagesToText(filenames, ofilename):
     f.close()
     return outfile
 
+# Get sentences from raw text
+def getSentencesFromRawText(text):
+  """Get sentences from raw text"""
+  sentences = sent_tokenize(text)
+  # Filter out sentences that have fewer than 3 words
+  sentences = [sentence.replace('\n',' ') for sentence in sentences if sentence.strip() in SECTION_TITLES or len(word_tokenize(sentence)) > 1]
+  return sentences
+
 # Get sentences from text file
 def getSentencesFromTextFile(fn):
+  """Get sentences from text file"""
   with open(fn, 'r') as file:
       text = file.read().replace('\n\n','. ')  # Replace newline characters with spaces
-      sentences = sent_tokenize(text)
-      # Filter out sentences that have fewer than 3 words
-      sentences = [sentence.replace('\n',' ') for sentence in sentences if sentence.strip() in SECTION_TITLES or len(word_tokenize(sentence)) > 1]
-
+      sentences = getSentencesFromRawText(text)
   return sentences
 
 # define utility for converting to proper gender format
@@ -152,6 +166,7 @@ def genderStringToGCloudGenderFormat(genderString):
 
 # convert text to speech using google cloud api
 def convertTextToSpeech(sentences, ofilename, voice_name="en-US-Studio-O", voice_gender=texttospeech.SsmlVoiceGender.FEMALE, language_code="en-US", audio_encoding=texttospeech.AudioEncoding.MP3):  
+  """Convert text to speech using google cloud api"""
   # Instantiates a client
   client = texttospeech.TextToSpeechClient()
   
@@ -170,7 +185,7 @@ def convertTextToSpeech(sentences, ofilename, voice_name="en-US-Studio-O", voice
 
   # Open the file in append mode so that
   # All contents of all sentence are added to the same file
-  f = open(outfile, "wb")
+  f = open(outfile, "ab")
   for idx, sentence in enumerate(tqdm(sentences, desc='Converting Text to Speech')):
     synthesis_input = texttospeech.SynthesisInput(text=sentence)
     try:
@@ -189,3 +204,70 @@ def convertTextToSpeech(sentences, ofilename, voice_name="en-US-Studio-O", voice
         raise e
   f.close()
   print(outfile + " prepared :) ")
+
+def query_gpt(prompt, instructions, gpt_model_type, max_tokens=1000, temperature=1):
+  COMPLETIONS_API_PARAMS = {
+      "temperature": temperature,
+      "max_tokens": max_tokens,
+      "model": gpt_model_type,
+  }
+  
+  response = openai.ChatCompletion.create(
+      messages = [{"role": "user", "content": prompt}, {"role": "system", "content": instructions}],
+      **COMPLETIONS_API_PARAMS
+  )
+  return response['choices'][0]['message']['content']
+
+def process_page(page_index, pdf_file, previous_section, gpt_model_type):
+    page = pdf_file[page_index]
+    page_text = page.get_text()
+    prompt_start = f"TEXT: \n\n{page_text}"
+    prompt_instructions = "Please use hypophora and anthypophora style of prose to convey your understanding of the passage. Your output should follow a call and reponse style of prose. For example: Why do we use this technique? Because it grabs the reader's attention."
+
+    num_input_tokens = len(tiktoken.encoding_for_model(gpt_model_type).encode(prompt_start + prompt_instructions))
+    gpt_out = query_gpt(prompt_start, prompt_instructions, gpt_model_type)
+    section_title = query_gpt(prompt_start, "What would be a good section title for this passage?", gpt_model_type)
+
+    updated_text = f'Page {page_index}, Continuing from Section {previous_section}, and beginning section {section_title}: \n\n{gpt_out}\n\n'
+    
+    num_output_tokens = len(tiktoken.encoding_for_model(gpt_model_type).encode(gpt_out))
+
+    return updated_text, section_title, num_input_tokens, num_output_tokens
+
+def reformulate_pdf(pdf_path, gpt_model_type='gpt-3.5-turbo', max_tokens=1000, temperature=1):
+  pdf_file = fitz.open(pdf_path)
+  text = ""
+  previous_section = "Pre-Abstract"
+  input_token_count = 0
+  output_token_count = 0
+
+  for page_index in range(len(pdf_file)):
+    updated_text, new_section, num_input, num_output = process_page(page_index, pdf_file, previous_section, gpt_model_type)
+    text += updated_text
+    input_token_count += num_input
+    output_token_count += num_output
+    previous_section = new_section
+
+  encoding = tiktoken.encoding_for_model(gpt_model_type)
+  tokens = encoding.encode(text)
+  return text, len(tokens), input_token_count, output_token_count
+
+def getSentencesAndConvertToSpeech(text_from_page, base_filename, voice_type, gender, language):
+  # This assumes you have a function to split text into sentences. 
+  sentences = getSentencesFromRawText(text_from_page)
+  # Text to Speech
+  convertTextToSpeech(sentences, base_filename, voice_type, gender, language)
+
+# calculate the amount of money spent by defining a function
+def calculateMoneySpent(input_token_count, output_token_count, gpt_model_type):
+  """Calculate the amount of money spent. Updated as of September 2023. Pricing data from https://openai.com/pricing/"""
+  # define the cost per token
+  if gpt_model_type is 'gpt-3.5-turbo':
+    cost_per_input_token = 0.0015/1000
+    cost_per_output_token = 0.002/1000
+  else:
+    raise NotImplementedError("Only gpt-3.5-turbo is supported at this time")
+
+  # calculate the cost
+  cost = input_token_count * cost_per_input_token + output_token_count * cost_per_output_token
+  return cost
